1bb46a6b5bd410940668a19eb228c8322797c9d5
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2025-02-27 16:41:21 -0300

    Lâmpadas

--- atividadeJS/atividade.js
+++ atividadeJS/atividade.js
@@ -3,3 +3,14 @@ function corAmarela(){
     let amarelo = document.getElementById('amarelo');
     amarelo.style.background = 'yellow'
 }
+
+function corVermelha(){
+    let amarelo = document.getElementById('amarelo');
+    amarelo.style.background = 'red'
+}
+
+function quebrar(){
+    let acesa = document.getElementById('acesa');
+    acesa.style.backgroundImage = 'url(Images/Lâmpada quebrada.png)'
+}
+
